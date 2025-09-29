ceb9aa7097ab3e3a411df0fc652f6c1ca72eefcb
cosineblast <55855728+cosineblast@users.noreply.github.com>
2025-09-29 13:59:37 -0300

    Tests are now randomly generated

--- tests.py
+++ tests.py
@@ -1,10 +1,8 @@
-# Things I want to work on:
-# 1. Better output for assertions, properly explaining what went wrong
-# 2. Better interface for defining test cases (possibily with builtin validation)
 
 import sys
 import thin_air
 import problems
+import random
 
 try:
     import rich.console
@@ -81,11 +79,19 @@ def get_test_cases():
 
     # The exercises and their respective comparing implementation go here
 
+    # base cases are cases which don't necessarily have to call `thin_air.solve`
+    # (usually base cases)
     def base(*example):
         return (tuple(example), "base")
 
+    # step cases are cases which necessarily expect calls to thin_air.solve
     def step(*example):
         return (tuple(example), "step")
+
+    random.seed(21)
+
+    def random_list(size=10):
+        return [random.randint(1, size * 2) for _ in range(size)]
 
     @exercise(
         [base(b, 0) for b in [2, 3, 10]]
@@ -99,61 +105,30 @@ def get_test_cases():
         return int(b**n)
 
     @exercise(
-        [
-            base([]),
-            step([1]),
-            step([2]),
-            step([3]),
-            step([1, 2, 30]),
-            step([3, 20, 1]),
-            step(list(range(2))),
-            step(list(range(4))),
-            step(list(range(8))),
-            step(list(range(16))),
-            step(list(reversed(range(100)))),
-        ]
+        [ base([]), step([1]), step([2]), step([3]), ] +
+        [step(random_list(size)) for size in [2,4,8,16,32,64,128,256]]
     )
     def list_sum(stuff):
         assert isinstance(stuff, list)
         return sum(stuff)
 
     @exercise(
-        [
-            base([1]),
-            base([2]),
-            base([-4]),
-            step([1, 2, 30]),
-            step([3, 20, 1]),
-            step([1, 2, 30]),
-            step([3, 20, 1]),
-            step([1, -2, 3]),
-            step([3, -4, 5]),
-            step(list(range(2))),
-            step(list(range(4))),
-            step(list(range(8))),
-            step(list(range(16))),
-            step(list(reversed(range(100)))),
-        ]
+        [ base([1]), base([2]), base([-4]), ] +
+        [ step(random_list(2 ** n)) for n in range(2, 16) ]
     )
     def list_max(stuff):
         assert isinstance(stuff, list)
         return max(stuff)
 
     @exercise(
-        [
-            base(1, []),
-            base(2, []),
-            base(-1, []),
-            base(0, []),
-            step(0, [1, 2, 3]),
-            base(1, [1, 2, 3]),
-            step(2, [1, 2, 3]),
-            step(3, [1, 2, 3]),
-            step(4, [1, 2, 3]),
-            base(10, [10, 20, 30]),
-            step(-1, [10, 20, 30]),
-            step(7, [0, 1, 1, 2, 3, 5, 8]),
-        ]
+        # empty list
+        [base(n, []) for n in random_list(10)] + 
+
+        # x is in the stuff
+        [(lambda stuff: base(random.choice(stuff), stuff))(random_list(2 ** n)) for n in range(2, 8)] +
+
+        # x is not in the stuff
+        [(lambda stuff: step(max(stuff) + 1, stuff))(random_list(2 ** n)) for n in range(2, 8)]
     )
     def list_element(x, stuff):
         assert isinstance(x, int)
@@ -195,6 +170,7 @@ def import_problem_or_none(name):
         return None
 
 def run_test_case(test):
+    global current_test
     name = test["name"]
     implementation = test["implementation"]
     examples = test["examples"]
@@ -253,8 +229,8 @@ def main():
         run_test_case(test)
 
 
-    console.print("[green] well done![/green].")
-    console.print(" i think you you should try [red]recursion[/red] ;)")
+    console.print("[green] well done![/green]")
+    console.print(" i think you you should try [red bold]recursion[/red bold] ;)")
 
 
 if __name__ == "__main__":
